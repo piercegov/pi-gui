67d2ca6283612c9427de487ceb79b64fb1b25297
Pierce Governale <piercegovernale@gmail.com>
2026-03-08 13:58:11 -0700

    React-scan for dev

--- apps/desktop/src/ui/main.tsx
+++ apps/desktop/src/ui/main.tsx
@@ -1,9 +1,14 @@
+import { scan } from "react-scan";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { initializeRpc } from "./lib/rpc-client";
 import { App } from "./app";
 import { PerfProfiler } from "./components/shell/perf-overlay";
+
+if (import.meta.env.DEV) {
+	scan({ enabled: true });
+}
 
 initializeRpc();
 
